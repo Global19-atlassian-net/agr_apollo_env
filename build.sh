19c3bf87fcab827f83ad4d58df397bc25b272fe4
Nathan Dunn <ndunn@me.com>
2017-10-20 21:18:50 -0700

    updated build.lsh

--- build.sh
+++ build.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 cd /apollo/ && \
-	rm -rf /scripts || true && \
-    mkdir /scripts || true && \
-    cp /apollo/docs/web_services/examples/groovy/*.groovy /scripts/ && \
+	rm -rf /tmp/scripts || true && \
+    mkdir /tmp/scripts || true && \
+    cp /apollo/docs/web_services/examples/groovy/*.groovy /tmp/scripts/ && \
     ./apollo clean-all && ./apollo deploy && \
     cp /apollo/target/apollo*.war /tmp/apollo.war && \
 	# So we can remove ~1.6 GB of cruft from the image. Ignore errors because cannot remove parent dir /apollo/
 	rm -rf /apollo/ || true && \
 	# Before moving back into a standardized location (that we have write access to)
-	mv /tmp/apollo.war /apollo/apollo.war
+	mv /tmp/apollo.war /apollo/apollo.war || true && \
+	mv /tmp/scripts /apollo/scripts
